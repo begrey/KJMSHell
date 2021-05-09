a361397525d7adabe8d9c9e1cc85a4176f3d5502
Hernameis <msh4287@naver.com>
2021-05-09 14:48:51 +0900

    parse.c fsanitize fixed before other_command

--- sunmin/srcs/init_env.c
+++ sunmin/srcs/init_env.c
@@ -3,6 +3,20 @@
 int		init_env(char *envp[])
 {
 	int		i;
+
+	i = 0;
+	while (envp[i])
+	{
+		ft_envadd_back(&env, ft_envnew(find_key(envp[i]), find_value(envp[i])));
+		i++;
+	}
+	return (0);
+}
+
+/*
+int		init_env(char *envp[])
+{
+	int		i;
 	int		len;
 	t_env	*temp;
 
@@ -12,7 +26,7 @@ int		init_env(char *envp[])
 	len = i;
 	temp = (t_env *)malloc(sizeof(t_env) * (len));
 	env = ft_envnew(find_key(envp[0]), find_value(envp[0]));
-	i = 0;
+	i = 1;
 	while (i < len)
 	{
 		temp[i].key = find_key(envp[i]);
@@ -23,5 +37,4 @@ int		init_env(char *envp[])
 	}
 	return (0);
 }
-
-
+*/
