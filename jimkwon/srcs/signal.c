a931f3301747946577b8347727f059b6e4ca2ccc
begrey <begrey@DESKTOP-FMSLMUF.localdomain>
2021-05-11 07:10:44 +0900

    signal

---
+++ jimkwon/srcs/signal.c
@@ -0,0 +1,30 @@
+#include <signal.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+ 
+void signalHandler(int sig){
+        if(sig==SIGINT){
+                printf("signal SIGINT : %d\n", sig);
+                //sleep(3000); //mac 에서는 sleep
+                exit(0);
+        }
+        if(sig==SIGTSTP){
+                printf("signal SIGTSTP : %d\n", sig);
+				exit(0);
+        }
+}
+int main(){
+		char *str;
+
+        signal(SIGINT, signalHandler);
+        signal(SIGTSTP, signalHandler);
+        printf("input Ctrl+C or Ctrl+Z \n");
+        while(1){
+			fgets(str,sizeof(str),stdin);
+			if (feof(stdin)){
+				printf("Ctrl+D exit \n");
+				exit(0);
+			}
+		}
+}
