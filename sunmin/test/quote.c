4824c68b597281017dd23367459c3b860b83571f
Hernameis <msh4287@naver.com>
2021-05-13 21:03:08 +0900

    split so hard.. plaese help me

--- sunmin/test/quote.c
+++ sunmin/test/quote.c
@@ -1,21 +1,98 @@
 #include "../srcs/minishell.h"
 
-int		get_word_num(const char *str)
+static int		is_space(const char c)
 {
-	int		num;
+	if (c == ' ' || (c >= 9 && c <= 13))
+		return (1);
+	return (0);
+}
+
+static char		flag_on(const char *str, char flag)
+{
 	char	*s;
 
 	s = (char *)str;
-	len = 0;
+	if ((!flag) && (*s == '\"' || *s == '\''))
+	{
+		if (*s == '\"')
+			flag = '\"';
+		else
+			flag = '\'';
+	}
+	return flag;
+}
+
+static char		flag_off(const char *str, char flag)
+{
+	char	*s;
+
+	s = (char *)str;
+	if (flag)
+	{
+		if (flag == '\"' && *s == '\"')
+			return 1;
+		else if (flag == '\'' && *s == '\'')
+			return 1;
+	}
+	return 0;
+}
+
+
+static int		get_len(const char *str)
+{
+	int		num;
+	int		word_num;
+	char	*s;
+	char	flag;
+	int		space;
+
+	word_num = 0;
+	if (!is_space(*s))
+		word_num = 1;
+	space = 0;
+	flag = '\0';
+	s = (char *)str;
+	num = 0;
+	while (*s && is_space(*s))		// 첫공백 넘기고 시작
+		s++;
 	while (*s)
 	{
-
+		if (!flag)
+		{
+			flag = flag_on(s, flag);
+			if (flag)
+				word_num++;
+		}
+		else
+			if (flag_off(s, flag))
+			{
+				flag = '\0';
+				space = 0;
+				word_num++;
+			}
+		if (!flag)
+		{
+			while (is_space(*s))		// 플래그가 없을 때 공백 넘겨
+			{
+				space = 1;
+				s++;
+			}
+			if (!is_space(*s) && space)
+			{
+				word_num++;
+				space = 0;
+			}
+		}
+		num++;
+		printf("@@%c@@ %d, %c, word_num %d\n", *s, num, flag, word_num);
 		s++;
 	}
+	if (flag)
+		printf("syntax error (quote)\n");
 	return (num);
 }
 
-void	ft_input(const char *str)
+static void		ft_input(const char *str)
 {
 	char	*s;
 	char	flag;
@@ -24,34 +101,16 @@ void	ft_input(const char *str)
 
 	flag = '\0';
 	s = (char *)str;
-	printf("[ft_input], %s\n", s);
-	num = get_word_num(str);
-
+	printf("[input], @@%s@@\n", s);
+	num = get_len(str);
+	printf("%d\n", num);
 	while (*s)
 	{
-		if (!flag)
-		{
-			while (*s == ' ')
-				s++;
-		}
-		if (!flag && (*s == '\"' || *s == '\'')) // 플래그 켜기
-		{
-			if (*s == '\"')
-				flag = '\"';
-			else
-				flag = '\'';
-		}
 
-
+		flag_on(s, flag);
 //		플래그 켜졌을 때, 꺼졌을 때
 
-		if (flag && (*s == '\"' || *s == '\''))									// 플래그 끄기
-		{
-			if (flag == '\"' && *str == '\"')
-				flag = 0;
-			if (flag == '\'' && *str == '\'')
-				flag = 0;
-		}
+		flag_off(s, flag);
 		s++;
 	}
 }
@@ -63,8 +122,8 @@ int		main(int argc, char **argv)
 	char	*test;
 	char	c;
 
-	test = ft_strdup("echo '\"'\" dd s\'s >a >>b <c minsungho | pwd ; ls");
-	printf("[main] input @@%s@@\n", argv[1]);
+	test = ft_strdup("  echo \"aaa\"b	  \'a\"aa\"\'");
+//	test = ft_strdup("echo '\"'\" dd s\'s >a >>b <c minsungho | pwd ; ls");
 	ft_input(test);
 	return (0);
 }
