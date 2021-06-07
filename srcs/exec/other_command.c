42ae6d0f930291a8246372713b28f8822c1929bb
Hernameis <msh4287@naver.com>
2021-06-07 13:16:20 +0900

    minishell stack solved

--- srcs/exec/other_command.c
+++ srcs/exec/other_command.c
@@ -6,7 +6,7 @@
 /*   By: jimkwon <jimkwon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/02 16:44:30 by jimkwon           #+#    #+#             */
-/*   Updated: 2021/06/03 18:08:35 by sunmin           ###   ########.fr       */
+/*   Updated: 2021/06/07 13:16:00 by sunmin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,11 +89,12 @@ void		other_command_exec(t_line *line, t_env *env, char *file_name)
 	char	**argv;
 	char	*temp_env;
 
+	argv = make_list_argv(line, file_name);
+	execve(line->arg, argv, NULL);
 	temp_env = extract_env("$PATH", env);
 	path = ft_split(temp_env, ':');
 	free(temp_env);
 	i = 0;
-	argv = make_list_argv(line, file_name);
 	while (path[i])
 	{
 		if ((ft_strncmp(path[i], line->arg, ft_strlen(path[i]))) != 0)
@@ -104,7 +105,6 @@ void		other_command_exec(t_line *line, t_env *env, char *file_name)
 		free(new_path);
 		i++;
 	}
-	execve(line->arg, argv, NULL);
 	exit_not_directory(argv);
 	not_found_command(argv, path, line, env);
 }
